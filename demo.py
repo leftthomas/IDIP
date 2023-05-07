ae37d981a537f68a3d56005d00dce11a04dd5b7c
leftthomas <leftthomas@qq.com>
2023-05-07 13:39:26 +0800

    make demo print better

--- demo.py
+++ demo.py
@@ -70,17 +70,18 @@ if __name__ == '__main__':
     if len(args.input) == 1:
         args.input = glob.glob(os.path.expanduser(args.input[0]))
         assert args.input, 'The input path(s) was not found'
-    for path in tqdm(args.input, disable=not args.output):
+    bar = tqdm(args.input, disable=not args.output)
+    for path in bar:
         # use PIL, to be consistent with evaluation
         img = read_image(path, format='BGR')
         start_time = time.time()
         predictions, visualized_output = demo.run_on_image(img)
-        logger.info('{}: {} in {:.2f}s'.format(path, 'detected {} instances'.format(len(predictions['instances']))
-        if 'instances' in predictions else 'finished', time.time() - start_time))
+        bar.set_description('{}: {} in {:.2f}s'.format(path, 'detected {} instances'.format(
+            len(predictions['instances'])) if 'instances' in predictions else 'finished', time.time() - start_time))
 
         if os.path.isdir(args.output):
             out_filename = os.path.join(args.output, os.path.basename(path))
         else:
-            assert len(args.input) == 1, 'Please specify a directory with args.output'
+            assert len(args.input) == 1, 'Please specify an existed directory with args.output'
             out_filename = args.output
         visualized_output.save(out_filename)
